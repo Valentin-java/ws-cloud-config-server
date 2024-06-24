22d6927a5017f2742f9ce3c915af232f9e226bde
Казанцев Валентин Игоревич <VIKazantsev@alfabank.ru>
2024-06-24 23:33:43 +0300

    init

--- src/main/java/com/workers/wscloudconfigserver/WsCloudConfigServerApplication.java
+++ src/main/java/com/workers/wscloudconfigserver/WsCloudConfigServerApplication.java
@@ -2,10 +2,12 @@ package com.workers.wscloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class WsCloudConfigServerApplication {
 
 	public static void main(String[] args) {
