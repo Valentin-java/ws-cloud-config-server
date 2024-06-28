810a0939f0b61b8e2783ee75b775c9d69a22b934
Казанцев Валентин Игоревич <VIKazantsev@alfabank.ru>
2024-06-28 09:21:27 +0300

    WRKS-0001: Fix

--- src/test/java/com/workers/wscloudconfigserver/WsCloudConfigServerApplicationTests.java
+++ src/test/java/com/workers/wscloudconfigserver/WsCloudConfigServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.workers.wscloudconfigserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class WsCloudConfigServerApplicationTests {
 
 	@Test
